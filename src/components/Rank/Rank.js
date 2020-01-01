3d93eab48174ac122f0c9730095932c7fc223d51
afeezdev <afeezdev@gmail.com>
2020-01-01 17:58:50 +0100

    ok

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const Rank = ({ name, entries}) => {
-    return (
-        <div>
-            <div className='white f3'>
-                {`${name}, your current entry count is...`}
-            </div>
-            <div className='white f1'>
+const Rank = ({ name, entries }) => {
+	return (
+		<div>
+			<div className="white f3">{`${name}, you have successfully signed in...`}</div>
+			{/* <div className='white f1'>
                 {entries}
-            </div>
-        </div>
-    )
-
-}
+            </div> */}
+		</div>
+	);
+};
 
 export default Rank;
